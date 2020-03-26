587b72d831a49af79f761d603b4698af60ea2307
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-26 18:03:41 +0100

    fatto seeder user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class UserTableSeeder extends Seeder
 {
@@ -10,7 +12,7 @@ class UserTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
       for ($i=0; $i < 4; $i++) {
 
@@ -18,6 +20,7 @@ class UserTableSeeder extends Seeder
         $newUser->name = $faker->name();
         $newUser->email = $faker->email;
         $newUser->password = $faker->email;
+        $newUser->save();
 
       }
     }
