b4bebf21b21530725fdeeaa145292d5259a83458
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-26 16:00:46 +0100

    up

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Posts</title>
+  </head>
+  <body>
+    @yield('header')
+    @yield('main')
+    @yield('footer')
+    @yield('script')
+  </body>
+</html>
