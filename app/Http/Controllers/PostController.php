57c288c0a6445e1a9582ec778d181cb96a1e744c
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-26 16:05:52 +0100

    up

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        dd($posts);
+        return view('posts.index', compact('posts'));
     }
 
     /**
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.layout')
+
+@section('header')
+ <h1>Posts index</h1>
+ @if(!empty($id))
+   <div>
+     Hai cancellato il post con ID: {{$id}}
+   </div>
+ @endif
+@endsection
+
+@section('main')
+  @foreach ($posts as $post)
+    <div class="room">
+      <ul>
+        <li>id: {{$post->id}}</li>
+        <li>Title: {{$post->title}}</li>
+        <li>Text: {{$post->content}}</li>
+        <li>Author: {{$post->author}}</li>
+        {{-- <li>
+          <form action="{{route('rooms.destroy', $room->id)}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">DELETE</button>
+          </form>
+        </li> --}}
+      </ul>
+    </div>
+  @endforeach
+@endsection
